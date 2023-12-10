f69bb3614f558da25a75452a5da431f3db67a4e0
jdabassett <jacobbassett@gmail.com>
2023-12-09 20:32:22 -0700

    still working on dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      './templates/**/*.html',
-      './node_modules/flowbite/**/*.js'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-      require('flowbite/plugin')
-  ],
+      content: [
+          './templates/**/*.html',
+          './node_modules/flowbite/**/*.js'
+      ],
+      darkMode: 'media',
+      theme: {
+        extend: {},
+      },
+      plugins: [
+          require('flowbite/plugin')
+      ],
 }
 
