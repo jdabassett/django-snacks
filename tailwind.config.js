022b6737c76a08d13a3962e0a9061dd788ed8430
jdabassett <jacobbassett@gmail.com>
2023-12-20 17:10:13 -0700

    removed .venv

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
           './templates/**/*.html',
           './node_modules/flowbite/**/*.js'
       ],
-      darkMode: 'media',
+      darkMode: 'class',
       theme: {
         extend: {},
       },
